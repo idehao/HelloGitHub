1719a0544d5594c859e79f69a45dbeda90fa72e0
Ivor <idehao@gmail.com>
2016-03-05 09:06:53 -0600

    Stack Navigation and passing variables
    Passing a value from one iOS screen to another screen

---
+++ HelloGitHub/ViewController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using UIKit;
+
+namespace HelloGitHub
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void BtnLogin_TouchUpInside (UIButton sender)
+		{
+			// NOTE: "HomeController" is the name of the controller that we wish to navigate to
+			// "home" is the Storyboard ID
+			HomeController controller = this.Storyboard.InstantiateViewController("home") as HomeController;
+			// TODO: add code to pass a value to the HomeController
+			controller.UserName = txtUser.Text;
+
+			this.NavigationController.PushViewController(controller, true);
+		}
+	}
+}
+
